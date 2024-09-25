eea954c2b4dc32731fa68390e06650a1925e7f99
Luke0213 <wongkinlun9527@gmail.com>
2024-09-25 10:02:56 +0800

    Prize.java

--- SuperLotto/src/main/Prize.java
+++ SuperLotto/src/main/Prize.java
@@ -7,25 +7,25 @@ public class Prize {
 		int FristPrizeNumber = First.FirstAwardNumber(First_Number, RandomNumbers_LinkedList);
 		int SecondPrizeNumer = Second.SecondAwardNumber(Second_Number, RandomNumbers_HashSet);
 		
-		if( FristPrizeNumber == 1 && SecondPrizeNumer == 1 ) {
+		if( FristPrizeNumber == 1 && SecondPrizeNumer >= 1 ) {
 			return "普獎";
 		}if( FristPrizeNumber == 3 && SecondPrizeNumer == 0 ) {
 			return "玖獎";
-		}if( FristPrizeNumber == 2 && SecondPrizeNumer == 1 ) {
+		}if( FristPrizeNumber == 2 && SecondPrizeNumer >= 1 ) {
 			return "捌獎";
-		}if( FristPrizeNumber == 3 && SecondPrizeNumer == 1 ) {
+		}if( FristPrizeNumber == 3 && SecondPrizeNumer >= 1 ) {
 			return "柒獎";
 		}if( FristPrizeNumber == 4 && SecondPrizeNumer == 0 ) {
 			return "陸獎";
-		}if( FristPrizeNumber == 4 && SecondPrizeNumer == 1 ) {
+		}if( FristPrizeNumber == 4 && SecondPrizeNumer >= 1 ) {
 			return "伍獎";
 		}if( FristPrizeNumber == 5 && SecondPrizeNumer == 0 ) {
 			return "肆獎";
-		}if( FristPrizeNumber == 5 && SecondPrizeNumer == 1 ) {
+		}if( FristPrizeNumber == 5 && SecondPrizeNumer >= 1 ) {
 			return "參獎";
 		}if( FristPrizeNumber == 6 && SecondPrizeNumer == 0 ) {
 			return "貳獎";
-		}if( FristPrizeNumber == 6 && SecondPrizeNumer == 1 ) {
+		}if( FristPrizeNumber == 6 && SecondPrizeNumer >= 1 ) {
 			return "頭獎";
 		}else {
 			return "未中獎";
