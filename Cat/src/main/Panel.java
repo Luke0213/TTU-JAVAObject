1fc29b9733c28b2c0f5e9853f543f2308fa50a12
Luke0213 <wongkinlun9527@gmail.com>
2024-09-30 11:19:36 +0800

    Panel.java

--- Cat/src/main/Panel.java
+++ Cat/src/main/Panel.java
@@ -8,7 +8,6 @@ public class Panel extends JPanel implements Runnable {
 
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
-    Thread CatThread;
     Button button;
 
     public Panel() {
@@ -23,16 +22,6 @@ public class Panel extends JPanel implements Runnable {
         this.addKeyListener(keyListener);
         this.setFocusable(true); 
         this.requestFocus();    
-    }
-
-    public void paintComponent(java.awt.Graphics g) {
-        super.paintComponent(g);
-        this.requestFocusInWindow();
-    }
-
-    public void launchCat() {
-        CatThread = new Thread(this);
-        CatThread.start();
     }
 
     public void run() {
